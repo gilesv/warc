d2dfec59fb3b86a04e6d4796c56e76b5a32c2fd4
gilesv <gilesv@protonmail.com>
2020-08-17 20:51:05 -0300

    1

--- fusion-wasm/lib/index.js
+++ fusion-wasm/lib/index.js
@@ -1,19 +1,21 @@
-import { Element, ElementProps as Props, render } from "../pkg/index.js";
-
-let tree = new Element("div", new Props("main", ""), [
-  new Element("h1", new Props("", ""), [new Element("_T", new Props("", "Hello wasm!"), [])])
-]);
+import { Element, ElementProps as Props, render } from "../pkg/fusion_wasm.js";
+let TEXT_ELEMENT = "_T";
+let NULL = "";
 
 function createElement(type, props = {}, ...rawChildren) {
-  props = {} || props;
+  props = props || {};
   let children = rawChildren.map(x => {
     if (typeof x === "string") {
-      return createElement("_T", null, { nodeValue: x });
+      return new Element(TEXT_ELEMENT, new Props(NULL, x), []);
     } else {
       return x;
     }
   }).filter(x => x instanceof Element);
-  let elementProps = new Props(props.className || "", props.nodeValue || "");
+
+  let elementProps = new Props(
+    props.className || NULL,
+    props.nodeValue || NULL
+  );
 
   return new Element(type, elementProps, children);
 }
--- fusion-wasm/src/lib.rs
+++ fusion-wasm/src/lib.rs
@@ -1,19 +1,12 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::convert::FromWasmAbi;
-use js_sys::Reflect;
 use web_sys::{Element as HTMLElement, Document};
+use js_sys::Reflect;
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace=console)]
     fn log(s: &str);
-}
-
-#[wasm_bindgen]
-#[derive(Clone)]
-pub struct ElementProps {
-    class_name: String,
-    node_value: String,
 }
 
 #[wasm_bindgen]
@@ -26,7 +19,11 @@ pub struct Element {
 #[wasm_bindgen]
 impl Element {
     #[wasm_bindgen(constructor)]
-    pub fn new(element_type: String, props: ElementProps, raw_children: Box<[JsValue]>) -> Element {
+    pub fn new(
+        element_type: String,
+        props: ElementProps,
+        raw_children: Box<[JsValue]>
+    ) -> Element {
         let children =
             raw_children
                 .iter()
@@ -37,12 +34,16 @@ impl Element {
         Element { element_type, props, children }
     }
 
-    fn from_js_value(js_value: &JsValue) -> Result<Element, JsValue> {
+    pub fn from_js_value(js_value: &JsValue) -> Result<Element, JsValue> {
         let ptr = unsafe { Reflect::get(&js_value, &JsValue::from_str("ptr"))? };
         let ptr_u32: u32 = ptr.as_f64().ok_or(JsValue::NULL)? as u32;
         let foo = unsafe { Element::from_abi(ptr_u32) };
 
         Ok(foo)
+    }
+
+    pub fn is_text_element(&self) -> bool {
+        self.element_type == "_T"
     }
 
     #[wasm_bindgen(getter)]
@@ -54,18 +55,13 @@ impl Element {
     pub fn props(&self) -> ElementProps {
         self.props.clone()
     }
+}
 
-    #[wasm_bindgen(getter)]
-    pub fn children(&self) -> usize {
-        self.children.len()
-    }
-
-    fn is_text_element(&self) -> bool {
-        unsafe {
-            log(&self.element_type());
-        }
-        self.element_type == "_T"
-    }
+#[wasm_bindgen]
+#[derive(Clone)]
+pub struct ElementProps {
+    class_name: String,
+    node_value: String,
 }
 
 #[wasm_bindgen]
@@ -75,14 +71,12 @@ impl ElementProps {
         ElementProps { class_name, node_value }
     }
 
-    #[wasm_bindgen(getter)]
     pub fn class_name(&self) -> String {
         self.class_name.clone()
     }
 
-    #[wasm_bindgen(setter)]
-    pub fn set_class_name(&mut self, value: String) {
-        self.class_name = value;
+    pub fn node_value(&self) -> String {
+        self.node_value.clone()
     }
 }
 
@@ -90,7 +84,6 @@ impl ElementProps {
 #[wasm_bindgen]
 pub fn render(js_element: JsValue, container: web_sys::Element) -> Result<(), JsValue> {
     let element = Element::from_js_value(&js_element).unwrap();
-    // let element: Element = unsafe { Element::from_abi(element_ptr) };
 
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
@@ -106,10 +99,10 @@ fn render_internal(
     document: &Document) -> Result<(), JsValue> {
 
     if element.is_text_element() {
-        let node = document.create_text_node(&element.props().node_value);
+        let node = document.create_text_node(&element.props().node_value());
         container.append_child(&node)?;
     } else {
-        let node = document.create_element(&element.element_type)?;
+        let node = document.create_element(&element.element_type())?;
 
         for child in element.children {
             render_internal(child, &node, document)?;
@@ -120,3 +113,68 @@ fn render_internal(
 
     Ok(())
 }
+
+struct Fiber {
+    
+}
+
+struct App {
+    wip_root: Option<Fiber>,
+    current_root: Option<Fiber>,
+    next_unit_of_work: Option<Fiber>,
+    wip_functional_fiber: Option<Fiber>,
+    hook_index: usize,
+}
+
+impl App {
+    fn new() -> Self {
+        App {
+            wip_root: None,
+            current_root: None,
+            next_unit_of_work: None,
+            wip_functional_fiber: None,
+            hook_index: 0
+        }
+    }
+
+    fn render(&mut self, element: Element, container: HTMLElement) {
+        todo!()
+    }
+
+    fn work_loop() {
+        todo!()
+    }
+
+    fn perform_unit_of_work(fiber: &Fiber) {
+        todo!()
+    }
+    
+    fn update_functional_tree(fiber: &Fiber) {
+        todo!()
+    }
+
+    fn update_regular_tree(fiber: &Fiber) {
+        todo!()
+    }
+
+    fn reconcile_children(current_fiber: &Fiber, children: Vec<Element>) {
+        todo!()
+    }
+
+    fn commit_work(fiber: &Fiber) {
+        todo!()
+    }
+
+    fn commit_node_append(fiber: &Fiber, parent_dom_node: HTMLElement) {
+        todo!()
+    }
+
+    fn commit_node_deletion(fiber: &Fiber, parent_dom_node: HTMLElement) {
+        todo!()
+    }
+
+    fn commit_root() {
+        todo!()
+    }
+}
+
